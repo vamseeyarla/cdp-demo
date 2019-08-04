75080e97d0f60bcfa5b12746789b800c544f856d
Attila Kanto <attila.kanto@gmail.com>
2019-08-04 13:03:50 +0200

    Refactoring Ram's scripts

--- scripts/env-scripts/setup.sh
+++ scripts/env-scripts/setup.sh
@@ -2,7 +2,9 @@
 
 set +x
 
-dp env create from-file --file cdp-demo1-env.json --profile dev
+export CB_PROFILE=dev
+
+dp env create from-file --file cdp-demo1-env.json --name cdp-demo1
 
 dp sdx create \
         --name cdp-demo1 \
@@ -11,13 +13,12 @@ dp sdx create \
         --cluster-shape LIGHT_DUTY \
         --cloud-storage-base-location "s3a://cloudbreak-group/user/rvenkatesh/dls/cdp-demo-dl-1-dev" \
         --cloud-storage-aws-instance-profile "arn:aws:iam::980678866538:instance-profile/mock-idbroker-assumer" \
-        --with-external-database \
-        --cidr "0.0.0.0/0" --profile dev
+        --with-external-database
 
-dp distrox create --cli-input-json de-cluster.json --name rv-test-1 --profile dev
+dp distrox create --cli-input-json de-cluster.json --name rv-test-1
 
-dp distrox create --cli-input-json de-with-all-cloudstorage.json --name rv-test-2 --profile dev
+dp distrox create --cli-input-json de-latest.json --name rv-de-latest-1
 
-dp distrox create --cli-input-json de-latest.json --name rv-de-latest-1 --profile dev
+dp distrox create --cli-input-json dm-cluster.json --name rv-dm-test-1
 
-dp distrox create --cli-input-json dm-latest.json --name rv-dm-latest-1 --profile dev
+dp distrox create --cli-input-json dm-latest.json --name rv-dm-latest-1
